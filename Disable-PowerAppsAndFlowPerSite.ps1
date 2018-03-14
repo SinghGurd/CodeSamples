b1f9308af1c5ddcafcf1deace5518535098930ba
SinghGurd <gurdevluck@gmail.com>
2018-03-15 10:53:49 +1300

    Create Disable-PowerAppsAndFlowPerSite.ps1

---
+++ Disable-PowerAppsAndFlowPerSite.ps1
@@ -0,0 +1,33 @@
+
+# Get all your sites
+$sites = Get-SPOSite -Limit All;
+$commitUpdates = $false;
+$log = @();
+
+
+foreach ($site in $sites) {
+  Write-Host "Processing --> $($site.Template), $($site.Url)" -ForegroundColor Green;
+  
+  # Determine if we need to act on the site
+	$disable = ($site.Template -in @("STS#0", "BLANKINTERNET#0") -and ($site.DisableFlows -ne "Disabled" -or $site.DisableAppViews -ne "Disabled"));
+  
+  if($disable -eq $true){
+		Write-Host "       Disabling Flow/PowerApps --> $($site.url)" -ForegroundColor Red;
+		if($commitUpdates -eq $true){
+			$site | Set-SPOSite -DisableAppViews Disabled -DisableFlows Disabled;
+		}
+	}	
+
+	$log += [PSCustomObject]@{
+		"Site"		= $site.Title
+		"Url"		= $site.Url
+		"CurrentStatusFlow" = $site.DisableFlows
+		"CurrentStatusPowerApps" = $site.DisableAppViews
+		"Template"	= $site.Template
+		"UpdatedToDisable" = $disable
+	};
+}
+
+$csvOutFilePath = < file system location>" + (Get-Date -Format FileDateTime) + ".csv";
+$log | Export-Csv $csvOutFilePath;
+Invoke-Item $csvOutFilePath;
